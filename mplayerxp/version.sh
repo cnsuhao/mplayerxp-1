2ffe7b0d888b0f0863f0a06418554d07915b8f90
nickols_k <nickols_k@92b12bc1-bfd6-414e-abbc-3841631887cf>
2009-12-04 19:00:15 +0000

    add trailer space

--- mplayerxp/version.sh
+++ mplayerxp/version.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # define here official release digits. Example: "1.0"
-version="0.8"
+version="0.8 "
 
 svn_exec=$(LC_ALL=C which svn)
 svn_revision=
 test -n "$svn_exec" && svn_revision=$(LC_ALL=C svn info 2>/dev/null | grep Revision | cut -d' ' -f2)
-test -z $svn_revision && svn_revision=$version || svn_revision="svn.$svn_revision"
+test -z $svn_revision && svn_revision=$version || svn_revision="svn.$svn_revision "
 
 cat << EOF > version.h
 #ifndef __VERSION_H
