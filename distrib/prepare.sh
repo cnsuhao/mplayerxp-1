b88d2f9e8d7c5de0bcbfec2c2e8ac14544232e5b
nickols_k <nickols_k@92b12bc1-bfd6-414e-abbc-3841631887cf>
2010-01-30 18:51:37 +0000

    more check-points during RPM building stage

--- distrib/prepare.sh
+++ distrib/prepare.sh
@@ -1,22 +1,47 @@
 #!/bin/sh
 
 version=$(echo `cat <../mplayerxp/version.sh | grep version= | cut -d '=' -f2 | cut -d '"' -f2`)
-rpm_src=$(echo `rpm --eval '%{_sourcedir}'`)
+rpm_src=$(rpm --eval '%{_sourcedir}')
 rpm_home="$rpm_src/../RPMS"
-srcdir=$(echo `pwd`)
+srcdir=$(pwd)
 destdir="$srcdir/upload"
+
+make_rpm() {
+target_arch=$1
+dest_rpm=$rpm_home/$target_arch/mplayerxp-$version-1_linux.$target_arch.rpm
+rpmbuild -bb --define "version $version" --define "_target_cpu $target_arch" --target $target_arch-linux mplayerxp.spec
+if ! test -f $dest_rpm ; then
+echo "$dest_rpm was not found. Exiting..."
+exit 1
+fi
+rpm_os=$(rpm -qp --queryformat "%{os}\n" $dest_rpm)
+rpm_arch=$(rpm -qp --queryformat "%{arch}\n" $dest_rpm)
+if test "$rpm_os" != "linux" ; then
+echo "$dest_rpm has wrong OS: $rpm_os. Exiting..."
+exit 1
+fi
+if test "$rpm_arch" != "$target_arch" ; then
+echo "$dest_rpm has wrong ARCH: $rpm_arch. Exiting..."
+exit 1
+fi
+echo "$dest_rpm has been built OK"
+}
 
 rpm2tgz() {
 target_arch=$1
 cd $rpm_home/$target_arch
+echo `pwd`
 mkdir tmp
-cp mplayerxp-$version-1_linux.$target_arch.rpm $destdir
-rpm --install --root $rpm_home/$target_arch/tmp --nodeps --ignorearch --ignoreos mplayerxp-$version-1_linux.$target_arch.rpm
+local srpm="mplayerxp-$version-1_linux.$target_arch.rpm"
+cp $srpm $destdir
+rpm --install --root $rpm_home/$target_arch/tmp --nodeps --ignorearch --ignoreos $srpm
+local stgz="mplayerxp-$version-1_linux.$target_arch.tgz"
 cd tmp
-tar c usr | gzip -9 >mplayerxp-$version-1_linux.$target_arch.tgz
-cp mplayerxp-$version-1_linux.$target_arch.tgz $destdir
+tar c usr | gzip -9 >$stgz
+cp $stgz $destdir
 cd ..
 rm -rf tmp
+cd $srcdir
 }
 
 if ! test -f "$rpm_src/mplayerxp-$version-src.tar.bz2" ; then
@@ -24,15 +49,25 @@ echo "Error: Source file: $rpm_src/mplayerxp-$version-src.tar.bz2 was not found!
 exit 1
 fi
 
-rpmbuild -bb --define "version $version" --target i686-linux mplayerxp.spec
-rpmbuild -ba --define "version $version" --target x86_64-linux mplayerxp.spec
-
 install -p -d $destdir
 
+# build ia32 binaries
+make_rpm i686
 rpm2tgz i686
+
+# build x86_64 binaries
+make_rpm x86_64
 rpm2tgz x86_64
 
+#build sources
+rpmbuild -bs --define "version $version" mplayerxp.spec
+if ! test -f $rpm_home/../SRPMS/mplayerxp-$version-1_linux.src.rpm ; then
+echo "mplayerxp-$version-1_linux.src.rpm was not found. Exiting..."
+exit 1
+fi
 cd $rpm_home/../SRPMS
 cp mplayerxp-$version-1_linux.src.rpm $destdir
 cd $rpm_src
 cp mplayerxp-$version-src.tar.bz2 $destdir
+
+echo "Everything seems OK"
