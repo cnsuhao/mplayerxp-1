dc52c95ede54b2b71a1fe4b117b140b88be84779
nickols_k <nickols_k@92b12bc1-bfd6-414e-abbc-3841631887cf>
2012-11-19 13:39:12 +0000

    constantization

--- mplayerxp/libmpstream/tvi/frequencies.h
+++ mplayerxp/libmpstream/tvi/frequencies.h
@@ -93,23 +93,23 @@ enum {
 /* --------------------------------------------------------------------- */
 
 struct CHANLIST {
-    char *name;
-    int   freq;
+    const char*	name;
+    int		freq;
 };
 
 struct CHANLISTS {
-    char             *name;
-    struct CHANLIST  *list;
-    int               count;
+    const char*			name;
+    const struct CHANLIST*	list;
+    int				count;
 };
 
 #define CHAN_COUNT(x) (sizeof(x)/sizeof(struct CHANLIST))
 
 /* --------------------------------------------------------------------- */
 
-extern struct CHANLISTS   chanlists[];
+extern const struct CHANLISTS   chanlists[];
 
 extern int                chantab;
-extern struct CHANLIST   *chanlist;
+extern const struct CHANLIST   *chanlist;
 extern int                chancount;
 #endif
